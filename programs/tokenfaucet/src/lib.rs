fc084e7d19b6f2e556bc2732db0bcd53a51ac385
joku <jokumusic@gmail.com>
2022-09-13 22:14:25 -0700

    Initial commit.

---
+++ programs/tokenfaucet/src/lib.rs
@@ -0,0 +1,82 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
+
+declare_id!("8omPPdRdNGtbeyTHn7zGhu7L3sXj2m2oesMrxE4fpTRh");
+
+const MINT_SEED_BYTES: &[u8] = b"mint";
+
+#[program]
+pub mod tokenfaucet {
+    use super::*;
+
+    pub fn create_airdrop_mint(_ctx: Context<CreateAirdropMint>, _decimals:u8) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn execute_airdrop(ctx: Context<ExecuteAirdrop>, amount: u64) -> Result<()> {
+        let token_program = ctx.accounts.token_program.to_account_info();
+        
+        let mint_to_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.recipient.to_account_info(),
+            authority: ctx.accounts.mint.to_account_info(),
+        };
+
+        let bump = *ctx.bumps.get("mint").unwrap();
+        
+        mint_to(
+            CpiContext::new_with_signer(
+                token_program, 
+                mint_to_accounts, 
+                &[&[
+                    MINT_SEED_BYTES,
+                    &[bump]
+                ]]
+            ), 
+            amount
+        )?;
+        
+        Ok(())
+    }
+}
+
+
+#[derive(Accounts)]
+#[instruction(_decimals:u8)]
+pub struct CreateAirdropMint<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    
+    #[account(
+        init, 
+        seeds = [MINT_SEED_BYTES], 
+        bump,
+        payer = signer,
+        mint::decimals = _decimals, 
+        mint::authority = mint
+    )]
+    pub mint: Account<'info, Mint>,
+    
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct ExecuteAirdrop<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    
+    #[account(mut, seeds = [MINT_SEED_BYTES], bump)]
+    pub mint: Account<'info, Mint>,
+    
+    #[account(mut)]
+    pub recipient: Account<'info, TokenAccount>,
+    
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
---
+++ tests/tokenfaucet.ts
@@ -0,0 +1,115 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Tokenfaucet } from "../target/types/tokenfaucet";
+import {PublicKey, Keypair, } from "@solana/web3.js";
+import * as spl_token from "@solana/spl-token";
+import { expect } from "chai";
+
+describe("tokenfaucet", () => {
+  const signerKeypair = Keypair.generate();
+  const lamportsRequired = 40000000;
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Tokenfaucet as Program<Tokenfaucet>;
+  const mintDecimals = 0;
+
+  let [mintAddress, mintAddressBump] = PublicKey.findProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode("mint"),
+    ], program.programId);
+
+  console.log('signer account: ', signerKeypair.publicKey.toBase58());
+
+  before(() => {
+    return new Promise<void>(async (resolve,reject) => {
+
+      console.log(`funding signer account with ${lamportsRequired} lamports...`);
+      
+      const airdropSignature = await provider.connection
+        .requestAirdrop(signerKeypair.publicKey, lamportsRequired)
+        .catch(reject);
+
+      if(!airdropSignature)
+        return;   
+
+      const airdropConfirmation = await provider.connection
+        .confirmTransaction(airdropSignature,'finalized')
+        .catch(reject);
+
+      if(!airdropConfirmation)
+        return;
+
+      resolve();
+    });
+
+  });
+
+  it("Create Mint", async () => {
+    console.log("creating mint: ", mintAddress.toBase58());
+
+    const existingMint = await spl_token
+      .getMint(provider.connection, mintAddress,'confirmed', spl_token.TOKEN_PROGRAM_ID)
+      .catch();
+
+    if(existingMint) {
+      console.log('mint already exists. skipping...');
+      return;
+    }
+
+    const tx = await program.methods
+      .createAirdropMint(mintDecimals)
+      .accounts({
+        signer: signerKeypair.publicKey,
+        mint: mintAddress,
+      })
+      .transaction();
+    
+      const txSignature = await anchor.web3.sendAndConfirmTransaction(provider.connection, tx, [signerKeypair], {commitment: 'finalized'});
+      
+      console.log("transaction signature", txSignature);
+
+      const mintAccount = await spl_token.getMint(provider.connection, mintAddress,'confirmed', spl_token.TOKEN_PROGRAM_ID);
+      expect(mintAccount.address).is.eql(mintAddress)
+      expect(mintAccount.decimals).is.equal(mintDecimals);
+      expect(mintAccount.supply).is.equal(BigInt(0));
+      expect(mintAccount.mintAuthority).is.eql(mintAddress);
+      expect(mintAccount.isInitialized).is.equal(true);    
+  });
+  
+it("Airdrop", async() => {
+    const airDropAmount = 1;
+
+    const signerTokenAccount = await spl_token.getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      signerKeypair,
+      mintAddress,
+      signerKeypair.publicKey,
+      false,
+      'finalized',
+      {commitment:'finalized'},
+      spl_token.TOKEN_PROGRAM_ID,
+      spl_token.ASSOCIATED_TOKEN_PROGRAM_ID);
+
+    console.log('signer token account: ', signerTokenAccount.address.toBase58());
+
+    const tx = await program.methods
+      .executeAirdrop(new anchor.BN(airDropAmount))
+      .accounts({
+        mint: mintAddress,
+        recipient: signerTokenAccount.address,
+        signer: signerKeypair.publicKey
+      })
+      .transaction();
+
+    const txSignature = await anchor.web3.sendAndConfirmTransaction(provider.connection, tx, [signerKeypair], {commitment: 'finalized'});
+    console.log("transaction signature", txSignature);
+
+    const updatedSignerTokenAccount = await spl_token.getAccount(provider.connection, signerTokenAccount.address,'confirmed', spl_token.TOKEN_PROGRAM_ID);
+    expect(updatedSignerTokenAccount.mint).is.eql(mintAddress);
+    expect(updatedSignerTokenAccount.address).is.eql(signerTokenAccount.address)
+    expect(updatedSignerTokenAccount.amount).is.equal(BigInt(airDropAmount));
+    expect(updatedSignerTokenAccount.owner).is.eql(signerKeypair.publicKey);
+  });
+  
+});
